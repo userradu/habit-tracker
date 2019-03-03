a6e866a6974fad3ad91b7d82ea0b476a7b67e2ba
usrrdu <usrrdu@gmail.com>
2019-03-03 10:40:29 +0200

    Style the login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['../shared/css/access.css']
 })
 export class LoginComponent implements OnInit {
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['../shared/css/access.css']
 })
 export class RegisterComponent implements OnInit {
 
