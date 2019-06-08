22d8ca291606b81cd08019ff95f8dc4d773fc193
radu <userradu91@gmail.com>
2019-06-08 21:20:09 +0300

    Fix apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-production: true,
-  apiUrl: "https://api-habit-tracker.herokuapp.com"
+  production: true,
+  apiUrl: "https://api-habit-tracker.herokuapp.com/api"
 };
